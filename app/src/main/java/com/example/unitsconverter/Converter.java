0d99abe09340b550b52ef674aea1620f7b8cf7f6
AlexiaBalentine <abalentine8@gmail.com>
2022-02-23 21:01:38 -0500

    working on sending data over

--- app/src/main/java/com/example/unitsconverter/Converter.java
+++ app/src/main/java/com/example/unitsconverter/Converter.java
@@ -1,14 +1,27 @@
 package com.example.unitsconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 import android.os.Bundle;
 
 public class Converter extends AppCompatActivity {
-
+    TextView messageText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_converter);
+        messageText = (TextView) findViewById(R.id.message);
+        String message =getIntent().getExtras().getString("radioChosen");
+        messageText.setText(message);
+
     }
 }
--- app/src/main/java/com/example/unitsconverter/MainActivity.java
+++ app/src/main/java/com/example/unitsconverter/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.unitsconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
+    String str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,4 +19,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+        switch (view.getId()) {
+            case R.id.radioButton:
+                if (checked)
+                    str = "button1Text";
+                break;
+            case R.id.radioButton2:
+                if (checked) str = "button2Text";
+                break;
+            case R.id.radioButton3:
+                if (checked) str = "button3Text";
+                break;
+            case R.id.radioButton4:
+                if (checked) str = "button4Text";
+                break;
+        }
+    }
+
+    public void clickedButton(View view) {
+        Intent intent = new Intent(this, Converter.class);
+        //EditText choosenRadio = (EditText) findViewById(R.id.);
+        intent.putExtra("radioChosen", str);
+        startActivity(intent);
+    }
 }
