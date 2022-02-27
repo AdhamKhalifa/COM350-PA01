1e05e859792910cd93a319ca23bcf77610206e27
Adham Khalifa <adham@adhamkhalifa.com>
2022-02-26 20:03:20 -0500

    Back button works

--- app/src/main/java/com/example/unitsconverter/Converter.java
+++ app/src/main/java/com/example/unitsconverter/Converter.java
@@ -2,6 +2,7 @@ package com.example.unitsconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import android.content.Intent;
@@ -32,7 +33,8 @@ public class Converter extends AppCompatActivity {
         //messageText = (TextView) findViewById(R.id.message);
         //String message =getIntent().getExtras().getString("radioChosen");
         //messageText.setText(message);
-
+        EditText measurementInput = (EditText) findViewById(R.id.measurement);
+        String measurementValue = measurementInput.getText().toString();
         gobackButton = (Button) findViewById(R.id.gobackButton);
         gobackButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,7 +43,16 @@ public class Converter extends AppCompatActivity {
             }
         });
 
+        Button converterBtn = (Button) findViewById(R.id.convertButton);
+        converterBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+
+            }
+        });
     }
+
+
 
     public void backtoMainActivity(){
         Intent intent = new Intent(this, MainActivity.class);
