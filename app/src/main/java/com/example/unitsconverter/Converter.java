a8a065b9f9b2146543717a4c6673be90938bcb83
AlexiaBalentine <abalentine8@gmail.com>
2022-02-26 19:59:56 -0500

    ~stable working on passing data + back button not working

--- app/src/main/java/com/example/unitsconverter/Converter.java
+++ app/src/main/java/com/example/unitsconverter/Converter.java
@@ -21,27 +21,8 @@ public class Converter extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_converter);
-        Intent intent = getIntent();
-        String selectedRadioValue = intent.getStringExtra("selectedRadioValue");
-        //String checked = intent.getStringExtra("checked");
-        messageText = (TextView) findViewById(R.id.messageChange);
-        messageText.setText(selectedRadioValue);
-        //messageText.setText(checked);
 
-
-        //messageText = (TextView) findViewById(R.id.message);
-        //String message =getIntent().getExtras().getString("radioChosen");
-        //messageText.setText(message);
-
-        gobackButton = (Button) findViewById(R.id.gobackButton);
-        gobackButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                backtoMainActivity();
-            }
-        });
-
-    }
+        }
 
     public void backtoMainActivity(){
         Intent intent = new Intent(this, MainActivity.class);
--- app/src/main/java/com/example/unitsconverter/MainActivity.java
+++ app/src/main/java/com/example/unitsconverter/MainActivity.java
@@ -12,56 +12,50 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
-    String str;
-    Button buttonSend;
+
+    RadioGroup radioGroupChoice;
+    Button send;
+    public Intent i;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        buttonSend =(Button) findViewById(R.id.button);
-        buttonSend.setOnClickListener(new View.OnClickListener() {
+        int choice1 = 1;
+        int choice2 = 2;
+        int choice3 = 3;
+        int choice4 = 4;
+        send = (Button) findViewById(R.id.button);
+        radioGroupChoice = (RadioGroup) findViewById(R.id.radioGroup);
+        i = new Intent(MainActivity.this,Converter.class);
+
+        radioGroupChoice.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
-                RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup);
-                String selectedRadioValue = ((RadioButton)findViewById(rg.getCheckedRadioButtonId() )).getText().toString();
-               //RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup);
-               Intent intent = new Intent(getApplicationContext(), Converter.class);
-                intent.putExtra("radioGroup1Selected", selectedRadioValue);
-                //int checked = rg.getCheckedRadioButtonId();
-                //String selectedRadioValue = ((RadioButton)findViewById(rg.getCheckedRadioButtonId() )).getText().toString();
-                //Intent intent = new Intent(MainActivity.this,Converter.class);
-                //intent.putExtra("checked",checked);
-                startActivity(intent);
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                switch(checkedId){
+                    case R.id.radioButton://
+                        i.putExtra("firstButton", choice1);
+                        break;
+                    case R.id.radioButton2://Radio button id
+                        i.putExtra("secondButton", choice2);
+                        break;
+                    case R.id.radioButton3://Radio button id
+                        i.putExtra("thirdButton", choice3);
+                        break;
+                    case R.id.radioButton4://Radio button id
+                        i.putExtra("fourthButton", choice4);
+                        break;
+                }
             }
         });
-
     }
 
-   // public void onRadioButtonClicked(View view) {
-       // boolean checked = ((RadioButton) view).isChecked();
-       // switch (view.getId()) {
-          //  case R.id.radioButton:
-                //if (checked)
-                  //  str = "button1Text";
-            //    break;
-          //  case R.id.radioButton2:
-               // if (checked) str = "button2Text";
-              //  break;
-          //  case R.id.radioButton3:
-              //  if (checked) str = "button3Text";
-               // break;
-           // case R.id.radioButton4:
-               // if (checked) str = "button4Text";
-               // break;
-       // }
-   // }
-    //
+        public void onClick(View view){
+            startActivity(i);
+        }
 
-    //public void clickedButton(View view) {
-      //  Intent intent = new Intent(this, Converter.class);
-        //EditText choosenRadio = (EditText) findViewById(R.id.);
-        //intent.putExtra("radioChosen", str);
-        //startActivity(intent);
-    }
+
+
+}
 
